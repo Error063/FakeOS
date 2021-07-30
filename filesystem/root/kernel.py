b23d3435302fe10a5c9958d17882e1b7588508cc
Error063 <2583080860@qq.com>
2021-07-30 10:30:17 +0800

    none

--- filesystem/root/kernel.py
+++ filesystem/root/kernel.py
@@ -9,12 +9,12 @@ threaddb.close()
 debug=True
 if not debug:
     username=None
-    print("Welcome to use UecatOS alpha 0.0.1\nWhen you want to exit, please type 'exit' to exit, not click 'X' on the window.\n")
+    print("Welcome to use FakeOS alpha 0.0.1\nWhen you want to exit, please type 'exit' to exit, not click 'X' on the window.\n")
     while username==None:
         username=login.login()
 else:
     username='root'
-    print("Welcome to use UecatOS alpha 0.0.1\nDebug Mode:True\nWhen you want to exit, please type 'exit' to exit, not click 'X' on the window.\n")
+    print("Welcome to use FakeOS alpha 0.0.1\nDebug Mode:True\nWhen you want to exit, please type 'exit' to exit, not click 'X' on the window.\n")
 
 commandlined.commandline(username)
 
