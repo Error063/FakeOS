3ee76c050a10cedf959dfdf058a69c6b32907ae9
Error063 <2583080860@qq.com>
2021-08-02 09:40:29 +0800

    Fluent Code

---
+++ filesystem/root/osinfo.py
@@ -0,0 +1,5 @@
+osVersion = '0.0.1'
+osBuild = '1'
+osAuthor = 'Error063'
+osCompany = 'Example Company'
+createTime = 1627625284
---
+++ filesystem/root/version.py
@@ -0,0 +1,12 @@
+def app(list):
+    import platform,osinfo
+    print(f'FakeOS {osinfo.osVersion} Build {osinfo.osBuild}')
+    print('===Host Information===')
+    print(f'Host:{platform.system()} {platform.release()} {platform.win32_edition()}')
+    print(f'Host CPU type:{platform.machine()}')
+    print('===Python Runtime Information===')
+    print(f'Python implementation:{platform.python_implementation()}')
+    print(f'Python version:{platform.python_version()}')
+    print(f'Python branch:{platform.python_branch()}')
+    print(f'Python compiler:{platform.python_compiler()}')
+    print(f'Python revision:{platform.python_revision()}')
